70612fa88d1c4d1c52df936c501f7a91485b6f79
Michel Boucey <michel.boucey@gmail.com>
2024-08-20 16:14:26 +0200

    nix files updated

--- ip6addr.nix
+++ ip6addr.nix
@@ -1,11 +1,13 @@
-{ mkDerivation, base, cmdargs, IPv6Addr, lib, text }:
+{ mkDerivation, base, IPv6Addr, lib, optparse-applicative, text }:
 mkDerivation {
   pname = "ip6addr";
   version = "2.0.0";
   src = ./.;
   isLibrary = false;
   isExecutable = true;
-  executableHaskellDepends = [ base cmdargs IPv6Addr text ];
+  executableHaskellDepends = [
+    base IPv6Addr optparse-applicative text
+  ];
   homepage = "https://github.com/MichelBoucey/ip6addr";
   description = "Commandline tool to deal with IPv6 address text representations";
   license = lib.licenses.bsd3;
